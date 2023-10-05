5af6844d2db056a85487dc2c7cf0d296edc65ba8
Eyal Lampel <eyal.lampel@gmail.com>
2023-10-05 18:09:28 +0300

    Added few dummy projects , made the project page a client component , changed FilterBar.tsx in such a way that the filtering is now working

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,31 +1,18 @@
+'use client';
 import FiltersBar from '@/components/Projects/FiltersBar/FiltersBar';
-import { Metadata } from 'next';
 import ProjectCard, {
   ProjectCardProps,
 } from '@/components/Projects/ProjectCard/ProjectCard';
+import { useState } from 'react';
 
-export const metadata: Metadata = {
-  title: 'הפרויקטים',
-  description:
-    'הכירו את פרויקטי הקוד הפתוח של חברי הקהילה, ובחרו לאילו פרויקטים תרצו להצטרף',
-  openGraph: {
-    title: 'הפרויקטים | מעקף',
-    description:
-      'הכירו את פרויקטי הקוד הפתוח של חברי הקהילה, ובחרו לאילו פרויקטים תרצו להצטרף',
-    url: 'https://maakaf-website.vercel.app/projects',
-    siteName: 'Maakaf',
-    type: 'website',
-    images: [
-      {
-        url: 'https://maakaf-website.vercel.app/favicon.ico',
-        width: 600,
-        height: 600,
-      },
-    ],
-  },
+type ProjectsPageProps = {
+  searchValue: string;
+  setSearchValue: (value: string) => void;
 };
 
-const ProjectsPage = () => {
+const ProjectsPage: React.FC = () => {
+  const [searchValue, setSearchValue] = useState('');
+
   const exampleProjectCardData: ProjectCardProps = {
     projectThumbnailSrc: '/',
     updatedDate: new Date('2023/3/14'),
@@ -47,11 +34,90 @@ const ProjectsPage = () => {
     discordLink: 'https://discord.gg/ayh8HtRQXk',
   };
 
+  const randomProjectCardData: ProjectCardProps = {
+    projectThumbnailSrc: '/path/to/random-thumbnail.jpg',
+    updatedDate: new Date('2023/5/10'),
+    createdDate: new Date('2023/4/1'),
+    projectName: 'פרויקט רנדומלי XYZ',
+    contributorCount: 80,
+    contributorAvatars: [
+      { imageSrc: '/images/avatars/avatar.jpg?0', name: 'אליהו הנביא' },
+      { imageSrc: '/images/avatars/avatar2.jpg?0', name: 'מרים לוי' },
+      { imageSrc: '/images/avatars/avatar3.jpg?0', name: 'יוסי כהן' },
+    ],
+    description: `פסקת תיאור של הפרויקט הרנדומלי. פרטים על הפרויקט, הטכנולוגיות שבו, המטרות והאתגרים.`,
+    tags: ['React', 'Node.js', 'MongoDB'],
+    githubLink: 'https://github.com/random/repo',
+    discordLink: 'https://discord.gg/randomlink',
+  };
+
+  const randomProjectCardData1: ProjectCardProps = {
+    projectThumbnailSrc: '/path/to/random-thumbnail1.jpg',
+    updatedDate: new Date('2023/6/15'),
+    createdDate: new Date('2023/5/5'),
+    projectName: 'פרויקט רנדומלי ABC',
+    contributorCount: 50,
+    contributorAvatars: [
+      { imageSrc: '/images/avatars/avatar3.jpg?0', name: 'דני רוזן' },
+      { imageSrc: '/images/avatars/avatar2.jpg?0', name: 'ליהי שפירא' },
+    ],
+    description: `תיאור של פרויקט ABC. פרטים על הטכנולוגיות, המטרות והאתגרים.`,
+    tags: ['Angular', 'Express', 'MySQL'],
+    githubLink: 'https://github.com/random/abc-repo',
+    discordLink: 'https://discord.gg/abc-link',
+  };
+
+  const randomProjectCardData2: ProjectCardProps = {
+    projectThumbnailSrc: '/path/to/random-thumbnail2.jpg',
+    updatedDate: new Date('2023/7/20'),
+    createdDate: new Date('2023/6/10'),
+    projectName: 'פרויקט רנדומלי DEF',
+    contributorCount: 70,
+    contributorAvatars: [
+      { imageSrc: '/images/avatars/avatar4.jpg?0', name: 'אילן גולדברג' },
+      { imageSrc: '/images/avatars/avatar3.jpg?0', name: 'שרה לוין' },
+    ],
+    description: `תיאור של פרויקט DEF. פרטים על הטכנולוגיות, המטרות והאתגרים.`,
+    tags: ['Vue.js', 'Django', 'PostgreSQL'],
+    githubLink: 'https://github.com/random/def-repo',
+    discordLink: 'https://discord.gg/def-link',
+  };
+
+  const randomProjectCardData3: ProjectCardProps = {
+    projectThumbnailSrc: '/path/to/random-thumbnail3.jpg',
+    updatedDate: new Date('2023/8/25'),
+    createdDate: new Date('2023/7/15'),
+    projectName: 'פרויקט רנדומלי GHI',
+    contributorCount: 60,
+    contributorAvatars: [
+      { imageSrc: '/images/avatars/avatar5.jpg?0', name: 'מיכל כהן' },
+      { imageSrc: '/images/avatars/avatar4.jpg?0', name: 'יואב רז' },
+    ],
+    description: `תיאור של פרויקט GHI. פרטים על הטכנולוגיות, המטרות והאתגרים.`,
+    tags: ['Svelte', 'Ruby on Rails', 'SQLite'],
+    githubLink: 'https://github.com/random/ghi-repo',
+    discordLink: 'https://discord.gg/ghi-link',
+  };
+
+  const allProjects = [
+    exampleProjectCardData,
+    randomProjectCardData,
+    randomProjectCardData1,
+    randomProjectCardData2,
+    randomProjectCardData3,
+  ];
+
+  const filteredProjects = allProjects.filter(project =>
+      project.projectName.includes(searchValue)
+  );
+
   return (
-    <section className="h-full">
-      <FiltersBar />
-      <ProjectCard {...exampleProjectCardData}></ProjectCard>
-    </section>
+      <section className="h-full">
+        <FiltersBar searchValue={searchValue} setSearchValue={setSearchValue} />
+        {filteredProjects.map(project => (
+            <ProjectCard key={project.projectName} {...project} />
+        ))}
+      </section>
   );
 };
 
